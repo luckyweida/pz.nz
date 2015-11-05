e5be17a057903788460109737037de02f0ede542
Weida <luckyweida@gmail.com>
2015-11-05 23:38:11 +1300

    fix vender

--- config.php
+++ config.php
@@ -5,13 +5,13 @@ define('CLIENT', 'Pozo Software Ltd');
 ini_set('magic_quotes_sybase', 0);
 date_default_timezone_set('Pacific/Auckland');
 define('CDN', 'http://index.net.nz/pozo');
-define('DOMAIN', 'http://' . $_SERVER['HTTP_HOST']);
+define('DOMAIN', 'http://localhost:3011');
 
 define('DEBUG_ENABLED', true);
 error_reporting(E_ALL);
 
 
-define('CMS', __DIR__ . '/../../../cms/v1.0/');
+define('CMS', __DIR__ . '/../../cms/v1.0/');
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'ckywgma30362com38023_pz');
