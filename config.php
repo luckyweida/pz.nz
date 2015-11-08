826031cfcc5a5d4751f288a88412acfbcbb89584
Weida <luckyweida@gmail.com>
2015-11-08 17:07:18 +1300

    config

--- config.php
+++ config.php
@@ -1,27 +1,47 @@
 <?php
 
-define('CLIENT', 'Pozo Software Ltd');
-
 ini_set('magic_quotes_sybase', 0);
 date_default_timezone_set('Pacific/Auckland');
-define('CDN', 'http://192.168.20.121:3003/pozo');
-define('DOMAIN', 'http://localhost:3011');
+define('CDN', 'http://final.nz');
+define('CMS', __DIR__ . '/../../cms/v1.0/');
+if (isset($_SERVER['HTTP_HOST'])) {
+    define('DOMAIN', 'http://' . $_SERVER['HTTP_HOST']);
+}
 
 define('DEBUG_ENABLED', true);
 error_reporting(E_ALL);
 
+define('DEV', true);
+if (DEV) {
+    //dev
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'ckywgma30362com38023_pz');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'er1c550n');
+    define('DB_CHAR', 'utf8');
 
-define('CMS', __DIR__ . '/../../cms/v1.0/');
+    define('SMTP_HOST', 'smtp.gmail.com');
+    define('SMTP_PORT', 587);
+    define('SMTP_USER', 'ns.gresource@gmail.com');
+    define('SMTP_PASS', 'weida911');
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'ckywgma30362com38023_pz');
-define('DB_USER', 'root');
-define('DB_PASS', 'er1c550n');
-define('DB_CHAR', 'utf8');
-define('IMAGICK', 'convert');
+    define('IMAGICK', 'convert');
+} else {
+    //live
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'ckywgma30362com38023_pz');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'er1c550n');
+    define('DB_CHAR', 'utf8');
 
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USER', 'ns.gresource@gmail.com');
-define('SMTP_PASS', 'weida911');
+    define('SMTP_HOST', 'smtp.gmail.com');
+    define('SMTP_PORT', 587);
+    define('SMTP_USER', 'ns.gresource@gmail.com');
+    define('SMTP_PASS', 'weida911');
+
+    define('IMAGICK', 'convert');
+}
+
+define('CLIENT', 'Pozo Software Ltd');
+
 
