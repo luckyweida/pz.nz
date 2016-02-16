cac6082a8b2dcf36f565ac5450bfe789b5d3dcfc
Weida <luckyweida@gmail.com>
2016-02-16 21:43:05 +1300

    asset

---
+++ src/Site/DAOs/Generated/ImageSize.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * 2016-02-16 21:11:47
+ */
+namespace Site\DAOs\Generated;
+
+class ImageSize extends \Pz\DAOs\Content {
+
+    function getFieldMap() {
+        global $CMS_METAS;
+        return array_merge(array(
+            'title' => 'title', 
+			'width' => 'extra1', 
+			'description' => 'description', 
+        ), array_combine($CMS_METAS, $CMS_METAS));
+    }
+
+    function getORMClass() {
+        return 'Pz\Entities\Content';
+    }
+
+    function getBaseQuery() {
+        return 'entity.modelId = 4';
+    }
+}
---
+++ src/Site/DAOs/ImageSize.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * 2016-02-16 20:53:42
+ */
+namespace Site\DAOs;
+
+class ImageSize extends \Site\DAOs\Generated\ImageSize {
+
+
+}
